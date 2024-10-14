42b687ae169fc1d46370980f9501fab4c43c821b
ALmiiiii <orillosdominique@gmail.com>
2024-10-14 20:51:25 +0800

    fixed carousel

--- carousel.js
+++ carousel.js
@@ -1,23 +1,25 @@
-const carouselInner = document.querySelector('.carousel-inner');
-const carouselImages = document.querySelectorAll('.carousel-image');
-const carouselPrev = document.querySelector('.carousel-prev');
-const carouselNext = document.querySelector('.carousel-next');
-
-let currentSlide = 0;
-const totalSlides = carouselImages.length;
-
-// Event listeners for controls
-carouselPrev.addEventListener('click', () => {
-    currentSlide = (currentSlide === 0) ? totalSlides - 1 : currentSlide - 1;
-    updateCarousel();
-});
-
-carouselNext.addEventListener('click', () => {
-    currentSlide = (currentSlide === totalSlides - 1) ? 0 : currentSlide + 1;
-    updateCarousel();
-});
-
-function updateCarousel() {
-    const imageWidth = carouselInner.clientWidth;
-    carouselInner.style.transform = `translateX(${-currentSlide * imageWidth}px)`;
-}
+var swiper = new Swiper(".centered-slide-carousel", {
+    centeredSlides: true,
+    paginationClickable: true,
+    loop: true,
+    spaceBetween: 30,
+    slideToClickedSlide: true,
+    pagination: {
+      el: ".centered-slide-carousel .swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      1920: {
+        slidesPerView: 4,
+        spaceBetween: 30
+      },
+      1028: {
+        slidesPerView: 2,
+        spaceBetween: 10
+      },
+      990: {
+        slidesPerView: 1,
+        spaceBetween: 0
+      }
+    }
+   });
