5cb6cdfb13c5b172a13383646bda9439623c58b0
unknown <alvarokyle13237@gmail.com>
2024-10-31 13:51:49 +0800

    Updated

--- script.js
+++ script.js
@@ -38,7 +38,6 @@ function toggleLoginFlyout(event) {
 
 // Dummy function for login submission
 function submitLogin() {
-    alert('Login submitted!');
     toggleLoginFlyout(); // Optionally close the flyout after submission
 }
 
@@ -50,7 +49,26 @@ window.onclick = function(event) {
     }
 }
 
-//admintable
+function toggleAdminFlyout(event) {
+    if (event) event.preventDefault(); // Prevent the default anchor behavior
+    const flyout = document.getElementById('AdminFlyout');
+    flyout.classList.toggle('active'); // Toggle the active class
+}
+
+// Dummy function for Admin Submissions
+function submitAdmin() {
+    toggleAdminFlyout(); // Optionally close the flyout after submission
+}
+
+// Close the login flyout when clicking outside of it
+window.onclick = function(event) {
+    const flyout = document.getElementById('AdminFlyout');
+    if (event.target === flyout) {
+        flyout.classList.remove('active');
+    }
+}
+
+//admin
 const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
 
 allSideMenu.forEach(item=> {
