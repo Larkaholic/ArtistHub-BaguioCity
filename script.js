93654f454f15f530365e00c1d4ef71d027eacb43
Harpoon <152266205+Larkaholic@users.noreply.github.com>
2024-10-31 13:50:59 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    if (window.scrollY > 50) { // You can adjust the scroll distance
-        header.style.backgroundColor = 'rgba(217, 217, 217, 0.3)'; // More transparent
-    } else {
-        header.style.backgroundColor = 'rgba(217, 217, 217, 1)'; // Solid gray
-    }
-});
