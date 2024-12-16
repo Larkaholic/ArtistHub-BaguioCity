475034be77b9f4d7bbc7c1810bac911223323cde
Larkaholic <orillosdominique@gmail.com>
2024-12-16 23:56:11 +0800

    fixed login in gallery, made gallery scrollable

--- script.js
+++ script.js
@@ -4,12 +4,6 @@ function toggleLoginFlyout(event) {
     flyout.classList.toggle('active'); // Toggle the active class
 }
 
-// Dummy function for login submission
-function submitLogin() {
-    alert('Login submitted!');
-    toggleLoginFlyout(); // Optionally close the flyout after submission
-}
-
 // Close the login flyout when clicking outside of it
 window.onclick = function(event) {
     const flyout = document.getElementById('LoginFlyout');
