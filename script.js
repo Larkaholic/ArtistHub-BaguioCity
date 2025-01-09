95cb7670601c303b22683af40a4287ebd2a98464
Larkaholic <orillosdominique@gmail.com>
2025-01-09 21:09:13 +0800

    fixed footer links

--- script.js
+++ script.js
@@ -39,7 +39,6 @@ function navToEvent(url) {
     window.location.href = url;
 }
 
-// Add this to your main JavaScript file
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
