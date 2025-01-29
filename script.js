b76b479a0a73ccc2dfe57a2f0af786687b001e81
Larkaholic <orillosdominique@gmail.com>
2025-01-29 16:27:44 +0800

    trying to fix navigation

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ window.onclick = function(event) {
     }
 }
 
-function navigateToEvent (){
-    window.location.href("events/events.html");
-}
+// function navigateToEvent (){
+//     window.location.href("events/events.html");
+// }
 
 function toggleNav() {
     const flyoutMenu = document.getElementById('flyout-menu');
@@ -35,9 +35,9 @@ function toggleForms() {
     }
 }
 
-function navToEvent(url) {
-    window.location.href = url;
-}
+// function navToEvent(url) {
+//     window.location.href = url;
+// }
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
