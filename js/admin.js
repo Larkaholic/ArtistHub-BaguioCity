7d7915fba197050fd212d61b8566d612073c2bed
Larkaholic <orillosdominique@gmail.com>
2025-04-09 22:54:16 +0800

    removed comments in admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,12 +1,9 @@
 import { checkAdminAccess } from '../js/utils.js';
 
-// Example of protecting an admin function
 async function someAdminFunction() {
     if (!(await checkAdminAccess())) return;
     
-    // Admin-only code here
     console.log('Performing admin action...');
 }
 
-// Make it globally available
 window.someAdminFunction = someAdminFunction; 
