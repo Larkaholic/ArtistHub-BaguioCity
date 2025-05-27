ecdc74bcbd971339763df099aed7c572c680bb9b
Larkaholic <orillosdominique@gmail.com>
2025-05-27 14:05:30 +0800

    removed event date svg

--- js/loadEvents.js
+++ js/loadEvents.js
@@ -152,11 +152,7 @@ function loadFloatingEvents(events, limit = 3) {
                     </svg>
                     <p>${event.location}</p>
                 </div>
-                <div class="flex items-center text-sm text-gray-600 mt-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                    </svg>
-                </div>
+                
             </div>
         </div>
     `).join('');
